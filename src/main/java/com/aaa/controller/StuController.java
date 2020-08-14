c054dcf321360c7249737da7a61ff99ae7abb427
wang <2041409629@qq.com>
2020-08-14 17:18:37 +0800

    sss

--- src/main/java/com/aaa/controller/StuController.java
+++ src/main/java/com/aaa/controller/StuController.java
@@ -3,5 +3,6 @@ package com.aaa.controller;
 public class StuController {
     public static void main(String[] args){
         System.out.println("测试2");
+        System.out.println("sss");
     }
 }
